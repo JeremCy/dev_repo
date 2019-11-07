318049345d6934f130f4dbf597643b0bee456dab
jerem <jeremie.cyrille@icloud.com>
2019-11-06 23:28:15 -0500

    JS1.1
    ajout formulaire+ anime js lier au formulaire

--- dev2/anim.js
+++ dev2/anim.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $(".page #nav ul li:first-child").click(function(){
       $("#contenu").slideDown("slow");
+      $("#formulaire").hide();
+    });
+    $("#test").click(function(){
+      $("#formulaire").slideDown("slow");
+      $("#contenu").hide();
     });
   });

9895e3d7dddb5584a179ef14d31a872d63fbe50a
jerem <jeremie.cyrille@icloud.com>
2019-11-09 20:02:53 -0500

    add map js end

--- dev2/anim.js
+++ dev2/anim.js
@@ -2,9 +2,24 @@ $(document).ready(function(){
     $(".page #nav ul li:first-child").click(function(){
       $("#contenu").slideDown("slow");
       $("#formulaire").hide();
+      $("#map").hide();
     });
     $("#test").click(function(){
       $("#formulaire").slideDown("slow");
       $("#contenu").hide();
+      $("#map").hide();
     });
-  });
+    $("#mp").click(function(){
+      $("#map").slideDown("slow");
+      $("#contenu").hide();
+      $("#formulaire").hide();
+      $("#map").ready(function myMap() {
+ // The location of Uluru
+ var uluru = {lat: -25.344, lng: 131.036};
+ // The map, centered at Uluru
+ var map = new google.maps.Map(
+     document.getElementById('map'), {zoom: 4, center: uluru});
+ 
+});
+});
+});

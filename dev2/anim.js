18bf7ad5ee3deb4380b38f61ac49b8c02a0de45d
jerem <jeremie.cyrille@icloud.com>
2019-11-09 22:40:39 -0500

    ''

--- dev2/anim.js
+++ dev2/anim.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $(".page #nav ul li:first-child").click(function(){
+  $(".page #nav ul li:first-child").click(function(){
       $("#contenu").slideDown("slow");
       $("#formulaire").hide();
       $("#map").hide();
-    });
+  });
     $("#test").click(function(){
       $("#formulaire").slideDown("slow");
       $("#contenu").hide();
@@ -14,12 +14,12 @@ $(document).ready(function(){
       $("#contenu").hide();
       $("#formulaire").hide();
       $("#map").ready(function myMap() {
- // The location of Uluru
- var uluru = {lat: -25.344, lng: 131.036};
- // The map, centered at Uluru
- var map = new google.maps.Map(
-     document.getElementById('map'), {zoom: 4, center: uluru});
- 
-});
-});
+        // The location of Uluru
+        var uluru = {lat: 46.3462, lng: -72.5788};
+        // The map, centered at Uluru
+       var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: uluru});
+      });
+    });
+
 });
